ca5ab0975f09283dda94643054fd9b7efaf9510d
Hayto98 <haytouchiha861@gmail.com>
2025-03-10 12:29:11 +0700

    code html

--- html-css/main.js
+++ html-css/main.js
@@ -1,1 +1,22 @@
 var ch = `ahihi đồ con chó`
+
+// làm quen với html 
+    // https://sustaining-heat-86b.notion.site/L-m-quen-v-i-HTML-1a056fc93432800c839ec6185ba4a235?pvs=4
+
+// các thẻ tiêu đề 
+    // https://sustaining-heat-86b.notion.site/C-c-th-ti-u-1a556fc9343280d0bdcbefd48bcc5c49?pvs=4
+
+// thẻ đoạn văn 
+    //https://sustaining-heat-86b.notion.site/Th-o-n-v-n-1a556fc9343280a99926ed6738673722?pvs=4
+
+// chữ đậm, chữ nghiêng 
+    //https://sustaining-heat-86b.notion.site/Ch-m-ch-nghi-ng-1a556fc9343280bd8d35d5fd7a43c354?pvs=4
+
+// HTML semantics 
+    //https://sustaining-heat-86b.notion.site/HTML-semantics-1a856fc93432806ca0cee9931108c17b?pvs=4
+
+// thẻ danh sách 
+    // https://sustaining-heat-86b.notion.site/Th-danh-s-ch-1a856fc934328025b4cfe7ddba8b35d9?pvs=4
+
+// thẻ liên kết 
+    // https://sustaining-heat-86b.notion.site/th-li-n-k-t-1ad56fc9343280d1b1ead55d80e0d1a7?pvs=4
